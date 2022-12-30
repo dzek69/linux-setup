46191bda10dd8b1485d788696291e7460cdb3eb6
dzek <git-public@dzek.eu>
2022-12-30 17:10:46 +0100

    init

---
+++ install.sh
@@ -0,0 +1,3 @@
+sudo apt-get update
+sudo apt install batcat screen mc lynx curl wget git fzf jq optipng
+sudo ln -s /usr/bin/batcat /usr/bin/bat
---
+++ yarn.sh
@@ -0,0 +1,5 @@
+yarn global add @dzek69/encode http-server localtunnel n svgo ts-library-template tldr
+echo "Suggested: wsl2-dns"
+
+echo "Add this to PATH:"
+yarn global dir
